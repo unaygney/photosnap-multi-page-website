05c4bcac51b530b294c6cf380b10c002949448c5
Guney Unay <guneymarketing17@hotmail.com>
2023-12-21 22:23:32 +0300

    added meta data

--- app/features/page.js
+++ app/features/page.js
@@ -1,5 +1,10 @@
 import React from "react";
 import FeaturesContainer from "@/containers/FeaturesContainer";
+
+export const metadata = {
+  title: "Features | Photosnap Multi Page Website",
+  description: "Photosnap Multi Page Website",
+};
 function Features() {
   return <FeaturesContainer />;
 }
--- app/pricing/page.js
+++ app/pricing/page.js
@@ -1,5 +1,11 @@
 import React from "react";
 import PricingComponent from "@/containers/PricingComponent";
+
+export const metadata = {
+  title: "Pricing | Photosnap Multi Page Website",
+  description: "Photosnap Multi Page Website",
+};
+
 function Pricing() {
   return <PricingComponent />;
 }
